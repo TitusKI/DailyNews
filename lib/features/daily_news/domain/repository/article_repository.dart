4dbb4ce420abb81868bcfac95c9b95669d91763c
TitusKI <kidumazie@gmail.com>
2024-10-06 16:52:56 +0300

    Top-Headlines and Everything

--- lib/features/daily_news/domain/repository/article_repository.dart
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -2,5 +2,29 @@ import 'package:daily_news/core/resources/data_state.dart';
 import 'package:daily_news/features/daily_news/domain/entities/article.dart';
 
 abstract class ArticleRepository {
+  Future<DataState<List<ArticleEntity>>> getAllNewsArticles();
+  // Top - Headlines via category
+//general
   Future<DataState<List<ArticleEntity>>> getNewsArticles();
+// business
+  Future<DataState<List<ArticleEntity>>> getBusinessArticles();
+// sports
+  Future<DataState<List<ArticleEntity>>> getSportsArticles();
+// technology
+  Future<DataState<List<ArticleEntity>>> getTechnologyArticles();
+// science
+  Future<DataState<List<ArticleEntity>>> getScienceArticles();
+// entertainmet
+  Future<DataState<List<ArticleEntity>>> getEntertaimentArticles();
+// health
+  Future<DataState<List<ArticleEntity>>> getHealthArticles();
+// Everything  sortBy
+// popularity
+  Future<DataState<List<ArticleEntity>>> getPopularArticles();
+// publishedAt recent
+  Future<DataState<List<ArticleEntity>>> getRecentArticles();
+
+  Future<List<ArticleEntity>> getSavedArticles();
+  Future<void> saveArticle(ArticleEntity article);
+  Future<void> removeArticle(ArticleEntity article);
 }
