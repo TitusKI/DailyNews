e8fce1e8bc82fbf330893ea6b03c339bbc30d234
TitusKI <kidumazie@gmail.com>
2024-10-09 16:55:15 +0300

    Data Access Object(DAO)

---
+++ lib/features/daily_news/data/data_source/local/DAO/article_dao.dart
@@ -0,0 +1,13 @@
+// DAO - Data Access Object
+import 'package:daily_news/features/daily_news/data/models/article_model.dart';
+import 'package:floor/floor.dart';
+
+@dao
+abstract class ArticleDao {
+  @Insert()
+  Future<void> insertArticle(ArticleModel article);
+  @delete
+  Future<void> deleteArticle(ArticleModel articleModel);
+  @Query('SELECT * FROM article')
+  Future<List<ArticleModel>> getArticles();
+}
